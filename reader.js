83bda96af635e8b15061664bb21c9e686d85b559
Keith Alexander <k.j.w.alexander@gmail.com>
2016-10-10 14:34:33 +0100

    Update reader.js

--- reader.js
+++ reader.js
@@ -1,8 +1,8 @@
-const Reader = run => ({
+export const Reader = run => ({
     map: f => Reader(env => f(run(env))),
     chain: f => Reader(env => f(Reader(run).run(env)).run(env)),
     ap: A => Reader(run).chain(f => A.map(f)),
     run
 })
 Reader.of = i => Reader(_=>i)
-const ask = Reader(i=>i).map
+export const ask = Reader(i=>i).map
