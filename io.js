18c71d23fbbb518a21b810317ae5e0190d98d1f9
Keith Alexander <k.j.w.alexander@gmail.com>
2016-10-17 21:28:18 +0100

    Update io.js

--- io.js
+++ io.js
@@ -2,7 +2,7 @@ const IO = xf => ({
   map: f => IO(_ => f(xf())),
   chain: f => IO(_=>f(IO(xf).run())),
   ap: A => A.chain(f => IO(xf).map(f)),
-  run: xf()
+  run: xf
 })
 IO.of = x => IO(_=>x)
 export default IO
