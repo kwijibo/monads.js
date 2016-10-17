9f318d24a8d9bf50cfcab0af16225b375f8d47ba
Keith Alexander <k.j.w.alexander@gmail.com>
2016-10-17 21:26:07 +0100

    Update io.js
    fantasy-land now states that `.ap` is passed the functor with the function, not the functor with the value

--- io.js
+++ io.js
@@ -1,6 +1,8 @@
 const IO = xf => ({
   map: f => IO(_ => f(xf())),
-  chain: f => IO(_=>f(IO(xf).run()))
+  chain: f => IO(_=>f(IO(xf).run())),
+  ap: A => A.chain(f => IO(xf).map(f)),
+  run: xf()
 })
-IO.of = IO
+IO.of = x => IO(_=>x)
 export default IO
