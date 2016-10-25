145917fde2024908e19610314842b0d2b45ad2cf
Keith Alexander <k.j.w.alexander@gmail.com>
2016-10-25 07:29:15 +0100

    added contramap

--- reader.js
+++ reader.js
@@ -1,5 +1,6 @@
 export const Reader = run => ({
     map: f => Reader(env => f(run(env))),
+    contramap: f => Reader(env => run(f(env))),
     chain: f => Reader(env => f(Reader(run).run(env)).run(env)),
     ap: A => Reader(run).chain(f => A.map(f)),
     run
