9a9ac57ea72976ff2c3bae440545b2cae35503ef
Keith Alexander <k.j.w.alexander@gmail.com>
2016-10-25 07:31:08 +0100

    swapped `ap` arguments
    inline with fantasy-land changes

--- reader.js
+++ reader.js
@@ -2,7 +2,7 @@ export const Reader = run => ({
     map: f => Reader(env => f(run(env))),
     contramap: f => Reader(env => run(f(env))),
     chain: f => Reader(env => f(Reader(run).run(env)).run(env)),
-    ap: A => Reader(run).chain(f => A.map(f)),
+    ap: A => A.chain(f => Reader(run).map(f)),
     run
 })
 Reader.of = i => Reader(_=>i)
