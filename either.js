7a48598ea0b1fb7278f65d03236ce9d89d39ca2e
Keith Alexander <k.j.w.alexander@gmail.com>
2017-02-01 11:37:34 +0000

    Create either.js

---
+++ either.js
@@ -0,0 +1,24 @@
+const Right = x => ({
+  constructor: Either,
+  map: f => Right(f(x)),
+  chain: f => f(x),
+  ap: F => F.map(f =>f(x)),
+  alt: E => Right(x),
+  reduce: (f, y) => f(y, x),
+  foldMap: (Of, Monoid) => Of(x),
+  cata: pattern => pattern.Right(x)
+})
+
+const Left = x => ({
+  constructor: Either,
+  map: Left(x),
+  chain: f => Left(x),
+  ap: F => Left(x),
+  alt: E => E,
+  reduce: (f, y) => f(y, x),
+  foldMap: (Of, Monoid) => Monoid,
+  cata: pattern => pattern.Right(x)
+})
+
+const Either = {of: Right, empty: Left}
+export Either
