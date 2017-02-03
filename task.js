01fbcd7db76ce7d408b5300057497d7d6483c06a
Keith Alexander <k.j.w.alexander@gmail.com>
2017-02-03 14:18:11 +0000

    Update task.js

--- task.js
+++ task.js
@@ -3,7 +3,20 @@ const Task = fork => ({
    chain: f => Task((reject, resolve)=>{ fork(reject, x => { f(x).fork(reject, resolve) })  }),
    join: () => Task(fork).chain(i=>i),
    ap: A => A.map(a => Task(fork).map(f => f(a))).join(),
+   concat: B => resolveFirst(Task(fork), B),
    fork
+})
+
+const resolveFirst = (A, B) => Task((reject, resolve) => {
+   var done = false
+   const guard = f => x => {
+     if(!done){
+       done = true
+       f(x)
+     }
+   }
+   A.fork(guard(reject), guard(resolve))
+   B.fork(guard(reject), guard(resolve))
 })
 Task.of = x => Task((_,resolve)=>{ resolve(x)})
 Task.empty = () => Task(()=>{})
