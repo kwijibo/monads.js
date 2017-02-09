37da1e85d0932798434bdc6ceb226208f491bd64
Keith Alexander <k.j.w.alexander@gmail.com>
2017-02-09 14:23:50 +0000

    Update maybe.js

--- maybe.js
+++ maybe.js
@@ -1,11 +1,23 @@
 const Just  = x => ({
+    isJust: true,
+    cata: patterns => patterns.Just(x),
     map: f => Just(f(x)),
+    filter: f => f(x)? Just(x) : Nothing(),
+    reduce: (reducer, accum) => reducer(accum, x),
     chain: f => f(x),
+    alt: A => Just(x),
+    ap: F => F.map(f => f(x)),
     getOrElse: fallback => x
 })
 const Nothing = () => ({
+    isJust: false,
+    cata: patterns => patterns.Nothing(),
     map: f => Nothing(),
+    filter: f => Nothing(),
+    reduce: (reducer, accum) => Nothing(),
     chain: f => Nothing(),
+    alt: A => A,
+    ap: F => Nothing(),
     getOrElse: fallback => fallback
 })
 
